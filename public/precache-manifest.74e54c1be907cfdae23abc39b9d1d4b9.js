d553556e2861052fe376f293c959a63fa4d4ec03
Christopher Adams <chrismichaeladams@gmail.com>
2019-03-31 10:30:50 -0400

    Add lock file

---
+++ public/precache-manifest.74e54c1be907cfdae23abc39b9d1d4b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fcbaa63ae9f2f057800",
+    "url": "/static/css/main.3c59c8d0.chunk.css"
+  },
+  {
+    "revision": "4fcbaa63ae9f2f057800",
+    "url": "/static/js/main.c8e96c20.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ce4759d56a492eb68a3",
+    "url": "/static/js/2.da6cff33.chunk.js"
+  },
+  {
+    "revision": "0cbe41d5dbffa7d7b8615fa4d5402b86",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
 const schema = require('./schema');
 
+const path = require('path'); // Allows working w/ file paths.
+
 const app = express();
 
 // Allow cross-origin / cors
@@ -12,6 +14,14 @@ app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
+
+// Set static folder for React and other files
+app.use(express.static('public'));
+
+// Send to the React index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
 
 const PORT = process.env.PORT || 5000
 
